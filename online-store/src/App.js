d3b2f59360e389b483f5f5052b73ef92c46f16ef
Nadav Mor <nadavmo>
2020-02-17 09:07:18 +0200

    basic layout and routing

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component'
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+
+
+const HatsPage = () => {
+ return (
+      <h1> Hats Page </h1>
+  );
+}
 
 function App() {
   return (
-    <HomePage></HomePage>
-  );
+     <div className='app-container'>
+       <Link to='/hats/1'>Link</Link>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/hats' component={HatsPage} />
+       <Route exact path='/hats/:id' component={HatsPage} />
+     </div>
+  )
 }
 
 export default App;
--- online-store/src/components/menu-item/menu-item.component.jsx
+++ online-store/src/components/menu-item/menu-item.component.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title }) => {
+const MenuItem = ({ title, imageUrl, size }) => {
 
 	return(
-		<div className='menu-item'>
-			<div className='content'>
+		<div className={`${size} menu-item`}
+			 style={{backgroundImage : `url(${imageUrl})`}}>
+			<Link to={'/' + title} className='content'>
 				<h1 className='title'>{title}</h1>
 				<span className='subtitle'>SHOP NOW</span>
-			</div>
+			</Link>
 		</div>
 	);
 }
