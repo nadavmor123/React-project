eb815586a3a877c6487212793687dc019af19428
nadav <nadavmor123@gmail.com>
2020-03-25 13:22:13 +0200

    updated sections

--- online-store/src/components/directory/directory.component.jsx
+++ online-store/src/components/directory/directory.component.jsx
@@ -3,64 +3,61 @@ import "./directory.styles.scss";
 import MenuItem from "../menu-item/menu-item.component";
 
 const Directory = () => {
-
   const [sections, setSections] = useState([]);
 
-  useEffect(()=>{
-
+  useEffect(() => {
     setSections([
-          {
-            title: "hats",
-            imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-            size: "small",
-            id: 1,
-            linkUrl: "shop/hats"
-          },
-          {
-            title: "jackets",
-            imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-            size: "small",
-            id: 2,
-            linkUrl: "shop/jackets"
-          },
-          {
-            title: "sneakers",
-            imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-            size: "small",
-            id: 3,
-            linkUrl: "shop/sneakers"
-          },
-          {
-            title: "womens",
-            imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-            size: "large",
-            id: 4,
-            linkUrl: "shop/womens"
-          },
-          {
-            title: "mens",
-            imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-            size: "large",
-            id: 5,
-            linkUrl: "shop/mens"
-          }
-        ])
-  });
+      {
+        title: "hats",
+        imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+        size: "small",
+        id: 1,
+        linkUrl: "shop/hats"
+      },
+      {
+        title: "jackets",
+        imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+        size: "small",
+        id: 2,
+        linkUrl: "shop/jackets"
+      },
+      {
+        title: "sneakers",
+        imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+        size: "small",
+        id: 3,
+        linkUrl: "shop/sneakers"
+      },
+      {
+        title: "womens",
+        imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+        size: "large",
+        id: 4,
+        linkUrl: "shop/womens"
+      },
+      {
+        title: "mens",
+        imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+        size: "large",
+        id: 5,
+        linkUrl: "shop/mens"
+      }
+    ]);
+  }, [sections]);
 
-    return (
-      <div className="directory-menu">
-        {sections.map(({ title, imageUrl, id, size, linkUrl }) => (
-          <MenuItem
-            key={id}
-            title={title}
-            imageUrl={imageUrl}
-            size={size}
-            linkUrl={linkUrl}
-          />
-        ))}
-      </div>
-    );
-
-}
+  return (
+    <div className="directory-menu">
+      {sections.map(({ title, imageUrl, id, size, linkUrl }) => (
+        <MenuItem
+          key={id}
+          title={title}
+          imageUrl={imageUrl}
+          size={size}
+          linkUrl={linkUrl}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Directory;
